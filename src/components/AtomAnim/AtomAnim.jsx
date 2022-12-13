1e29a04ec0365bd75aa59fa35d596446dc72cbb7
Danila Aleksin <danya09kz@gmail.com>
2022-12-14 05:08:19 +0600

    optimize glb model

--- src/components/AtomAnim/AtomAnim.jsx
+++ src/components/AtomAnim/AtomAnim.jsx
@@ -1,31 +1,32 @@
+import React from 'react'
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { useGLTF, Bounds, OrbitControls, Float } from '@react-three/drei'
 
 
-export function AtomAnim() {
+export const AtomAnim = React.memo( function AtomAnim() {
     
     return (
-        <Canvas shadows>
-            <directionalLight position={[1, 3, 2]} castShadow />
-            <hemisphereLight intensity={1.5} groundColor="#ffffff" />
+        <Canvas>
+            <directionalLight position={[1, 3, 2]} />
+            <hemisphereLight intensity={1.2} groundColor="#ffffff" />
             <Suspense fallback={null}>
                 <Bounds fit clip observe>
                     <Float speed={10} floatingRange={[0.2, 0]}>
-                        <Atom invert />
+                        <Atom />
                     </Float>
                 </Bounds>
             </Suspense>
-            <OrbitControls makeDefault enableZoom={false} />
+            <OrbitControls makeDefault enableZoom={false} enablePan={false} />
         </Canvas>
     )
-}
+})
 
-function Atom({props}) {
+function Atom() {
     const { nodes } = useGLTF('/react-transformed.glb')
     return (
-        <mesh castShadow receiveShadow geometry={nodes.atom.geometry} {...props} dispose={null}>
-            <meshPhongMaterial color="#006fe6" emissive="#000000" specular="#00c0f0" shininess="0" wireframe/>
+        <mesh geometry={nodes.atom.geometry} >
+            <meshPhongMaterial color="#006fe6" emissive="#000000" specular="#00c0f0" shininess="0"/>
         </mesh>
     )
 }
